173e80fa19f0c9fbe23c0b3da002472735c8fc16
durveshsunilshirsath-design <durveshsunilshirsath@gmail.com>
2026-02-18 10:05:36 +0530

    array.c

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	int b[10];
+	printf("Enter Array Elements:");
+	for(i=0;i<10;i++){
+	   scanf("%d",&b[i]);
+    }
+	for(i=0;i<10;i++){
+		printf("%d\n",b[i]);
+	
+	}
+return 0;
+}
